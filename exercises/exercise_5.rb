1a7f8113aaecd07023e45c350bc80807db822716
cdmac94 <collinmacleod79@gmail.com>
2022-02-01 18:05:49 +0000

    Added function to find total and average annual income and stores where annual income is over 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_company_revenue = Store.sum("annual_revenue")
+puts "Total Company Revenue: #{@total_company_revenue}"
+
+@avg_revenue = @total_company_revenue / Store.count
+puts "Average Annual Revenue: #{@avg_revenue}"
+
+@store_over_1M = Store.where("annual_revenue > 1000000")
+puts "Stores with Annual Revenue over 1M: #{@store_over_1M}"
